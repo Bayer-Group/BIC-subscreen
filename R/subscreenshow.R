1b5b0478f6b4fc0bbe8d4098f47c3d5f47b13fb2
stjeske <92299058+stjeske@users.noreply.github.com>
2024-08-12 13:20:05 +0200

    Update subscreenshow.R
    Update to Version 4.0.3

--- R/subscreenshow.R
+++ R/subscreenshow.R
@@ -16,6 +16,15 @@
 #' @param graphSubtitle subtitle for explorer plot
 #' @param favour_label_verum_name verum name for label use in explorer graph
 #' @param favour_label_comparator_name comparator name for label use in explorer graph
+#' @param showTables logical for display tables in 'Explorer tab'
+#' @param reference_line_at_start logical for reference line appearance at start (TRUE/FALSE)
+#' @param reference_value numeric value of horizontal reference line
+#' @param favour_label_at_start logical for favour labels appearance at start (TRUE/FALSE)
+#' @param favour_direction logical for favor label direction (TRUE/FALSE) where TRUE means
+#'        favour_label_verum_name is on top
+#' @param subgroup_levels_at_start integer value for subgroup level slider at start
+#' @param yaxis_type character ("lin"/"log") for yaxis type
+#' @param add_funnel_at_start logical for funnel appearance at start (TRUE/FALSE)
 #'
 #' @keywords subgroup analysis visualization
 #' @import shiny
@@ -31,7 +40,15 @@ subscreenshow <- function (
     windowTitle = "Subgroup Explorer",
     graphSubtitle = NULL,
     favour_label_verum_name = NULL,
-    favour_label_comparator_name = NULL
+    favour_label_comparator_name = NULL,
+    showTables = FALSE,
+    reference_line_at_start = FALSE,
+    reference_value = 1,
+    favour_label_at_start = FALSE,
+    favour_direction = TRUE,
+    subgroup_levels_at_start = NA,
+    yaxis_type = "lin",
+    add_funnel_at_start = FALSE
   ){
 
   ####.. warnings ####
@@ -71,11 +88,50 @@ subscreenshow <- function (
     stop("Could not find Shiny app directory. Please try to re-install `subscreen`.", call. = FALSE)
   }
 
+
+  if (yaxis_type != "log" & yaxis_type != "lin") {
+    cat("Error: parameter yaxis_type needs to be 'lin' or 'log' in subscreenshow")
+    stop()
+  }
+  if (!is.logical(reference_line_at_start)) {
+    cat("Error: parameter reference_line_at_start needs to be logical (TRUE/FALSE) in subscreenshow")
+    stop()
+  }
+  if (!is.logical(favour_label_at_start)) {
+    cat("Error: parameter favour_label_at_start needs to be logical (TRUE/FALSE) in subscreenshow")
+    stop()
+  }
+  if (!is.logical(favour_direction)) {
+    cat("Error: parameter favour_direction needs to be logical (TRUE/FALSE) in subscreenshow")
+    stop()
+  }
+  if (!is.logical(add_funnel_at_start)) {
+    cat("Error: parameter add_funnel_at_start needs to be logical (TRUE/FALSE) in subscreenshow")
+    stop()
+  }
+  if (!is.numeric(reference_value)) {
+    cat("Error: parameter reference_value needs to be numeric in subscreenshow")
+    stop()
+  }
+  if(!is.na(subgroup_levels_at_start)) {
+    if (!subgroup_levels_at_start %% 1 == 0) {
+      cat("Error: parameter subgroup_levels_at_start needs to be integer in subscreenshow")
+      stop()
+    }
+  }
   apppars <- list(
     scresults = scresults,
     scresults_name = deparse(substitute(scresults)),
     variable_importance = variable_importance,
-    NiceNumbers = NiceNumbers
+    NiceNumbers = NiceNumbers,
+    showTables = showTables,
+    reference_line_at_start = reference_line_at_start,
+    reference_value = reference_value,
+    favour_label_at_start = favour_label_at_start,
+    favour_direction = favour_direction,
+    yaxis_type = yaxis_type,
+    subgroup_levels_at_start = subgroup_levels_at_start,
+    add_funnel_at_start = add_funnel_at_start
   )
 
   ui <- server <- NULL
