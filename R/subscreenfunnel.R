98959adfe925715f17aa1d0b9640569743ce61d1
VSchmidtBayer <131263669+VSchmidtBayer@users.noreply.github.com>
2025-09-29 08:00:56 +0200

    Update subscreenfunnel.R
    - change n to .data$n in the group_by statements
    - add package prefix dplyr:: to across and everything function calls

--- R/subscreenfunnel.R
+++ R/subscreenfunnel.R
@@ -102,9 +102,9 @@ subscreenfunnel <- function(
 
   tmp3 <- tmp2 %>%
     dplyr::as_tibble() %>%
-    dplyr::group_by(n) %>%
-    dplyr::reframe(across(everything(), function(x) quantile(x, probs = c(alpha/2, 1-(alpha/2)), na.rm = TRUE))) %>%
-    dplyr::group_by(n) %>%
+    dplyr::group_by(.data$n) %>%
+    dplyr::reframe(dplyr::across(dplyr::everything(), function(x) quantile(x, probs = c(alpha/2, 1-(alpha/2)), na.rm = TRUE))) %>%
+    dplyr::group_by(.data$n) %>%
     dplyr::mutate(alpha = c(alpha/2, 1-(alpha/2))) %>%
     dplyr::ungroup()
 
